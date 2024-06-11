02c2e4fb4acfc031f7cd6fef3e39dcac8c0c8082
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-06-11 17:12:38 +0600

    Cors Origin

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const { MongoClient, ObjectId } = require("mongodb");
 
 const cors = require("cors");
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	next();
+});
+
 app.use(cors({ origin: "https://todo-module-3.vercel.app" }));
 
 app.use(express.json());
